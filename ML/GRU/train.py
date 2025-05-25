953dd05b97cc4805277c5ae365479d183e4a1313
james-luong <83560549+james-luong@users.noreply.github.com>
2025-05-25 22:56:04 +1000

    Update train.py

--- ML/GRU/train.py
+++ ML/GRU/train.py
@@ -1,7 +1,8 @@
 """
 Train the NN model.
+Run Command: python3 train.py --model <model_name> --epochs <number_of_epochs>
 """
-import sys
+import sys, os
 import warnings
 import argparse
 import numpy as np
@@ -33,78 +34,35 @@ def train_model(model, X_train, y_train, name, config):
         epochs=config["epochs"],
         validation_split=0.05)
 
-    model.save('model/' + name + '.h5')
+    model.save('ML/GRU/model/' + name + '.h5')
     df = pd.DataFrame.from_dict(hist.history)
-    df.to_csv('model/' + name + ' loss.csv', encoding='utf-8', index=False)
-
-
-def train_seas(models, X_train, y_train, name, config):
-    """train
-    train the SAEs model.
-
-    # Arguments
-        models: List, list of SAE model.
-        X_train: ndarray(number, lags), Input data for train.
-        y_train: ndarray(number, ), result data for train.
-        name: String, name of model.
-        config: Dict, parameter for train.
-    """
-
-    temp = X_train
-    # early = EarlyStopping(monitor='val_loss', patience=30, verbose=0, mode='auto')
-
-    for i in range(len(models) - 1):
-        if i > 0:
-            p = models[i - 1]
-            hidden_layer_model = Model(input=p.input,
-                                       output=p.get_layer('hidden').output)
-            temp = hidden_layer_model.predict(temp)
-
-        m = models[i]
-        m.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
-
-        m.fit(temp, y_train, batch_size=config["batch"],
-              epochs=config["epochs"],
-              validation_split=0.05)
-
-        models[i] = m
-
-    saes = models[-1]
-    for i in range(len(models) - 1):
-        weights = models[i].get_layer('hidden').get_weights()
-        saes.get_layer('hidden%d' % (i + 1)).set_weights(weights)
-
-    train_model(saes, X_train, y_train, name, config)
+    df.to_csv('ML/GRU/model/' + name + ' loss.csv', encoding='utf-8', index=False)
 
 
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model",
-        default="lstm",
+        default="gru",
         help="Model to train.")
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=30,
+        help="Number of epochs to train the model (Default: 30).")
     args = parser.parse_args()
 
     lag = 12
-    config = {"batch": 256, "epochs": 30}
-    file1 = 'data/train.csv'
-    file2 = 'data/test.csv'
+    config = {"batch": 256, "epochs": args.epochs}
+    file1 = os.path.join(os.path.dirname(__file__), "data/train.csv")
+    file2 = os.path.join(os.path.dirname(__file__), "data/test.csv")
     X_train, y_train, _, _, _ = process_data(file1, file2, lag)
 
-    if args.model == 'lstm':
-        X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-        # Model structure: [input_sequence_length, first_hidden, second_hidden, output_units]
-        m = model.get_lstm([12, 64, 64, 1])
-        train_model(m, X_train, y_train, args.model, config)
     if args.model == 'gru':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
         # Model structure: [input_sequence_length, first_hidden, second_hidden, output_units]
         m = model.get_gru([12, 64, 64, 1]) 
         train_model(m, X_train, y_train, args.model, config)
-    if args.model == 'saes':
-        X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1]))
-        m = model.get_saes([12, 400, 400, 400, 1])
-        train_seas(m, X_train, y_train, args.model, config)
 
 
 if __name__ == '__main__':
