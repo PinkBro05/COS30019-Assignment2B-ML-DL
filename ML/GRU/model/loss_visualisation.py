b1662d2d46dc3171fbe3f42b750f9dc1cdef1bae
james-luong <83560549+james-luong@users.noreply.github.com>
2025-05-25 23:04:45 +1000

    Create loss_visualisation.py

---
+++ ML/GRU/model/loss_visualisation.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import csv
+import os
+
+data = os.path.join(os.path.dirname(os.path.dirname(__file__)),'model/gru loss.csv')
+
+reader = csv.reader(open(data, 'r'))
+loss = []
+val_loss = []
+for row in reader:
+    if reader.line_num == 1:
+        continue
+    else:
+        loss.append(float(row[0]))
+        val_loss.append(float(row[2]))
+
+loss_data = [loss, val_loss]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+for i, data in enumerate(loss_data):
+    label = 'Loss' if i == 0 else 'Validation Loss'
+    ax.plot(data, marker='o', linestyle='-', label=label)
+plt.plot(loss, marker='o', linestyle='-', color='b')
+plt.title('GRU Loss Over Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(True)
+plt.show()
